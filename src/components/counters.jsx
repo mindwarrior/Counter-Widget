0543b97e5e6efa14057b5918e7e7beaf3f9227be
Kartik Gera <kartik91827@gmail.com>
2019-03-04 15:42:04 -0500

    Destructuring Arguments

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  
   render() {
+    const { onReset, counters, onDelete, onIncrement } = this.props;
     return (
       <div>
         <button
-          onClick={this.props.onReset}
+          onClick={onReset}
           className="btn btn-primary btn-sm m-2"
         >
           Reset
@@ -15,8 +15,8 @@ class Counters extends Component {
         {this.props.counters.map(counter => (
           <Counter
             key={counter.id}
-            onDelete={this.props.onDelete}
-            onIncrement={this.props.onIncrement}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
             counter={counter}
           />
         ))}
